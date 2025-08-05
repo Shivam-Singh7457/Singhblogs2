a38b87f6714e783c578dc8fd01255c7d64af8981
Shivam Singh <shivamsinghsn95@gmail.com>
2025-08-06 00:27:06 +0530

    Update firebase.js appspot

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAezdN0iKR2goLZT7KvWTUfJSzOrfL9n80",
   authDomain: "singhblogs-123.firebaseapp.com",
   projectId: "singhblogs-123",
-  storageBucket: "singhblogs-123.firebasestorage.app",
+  storageBucket: "singhblogs-123.appspot.com",
   messagingSenderId: "972914232528",
   appId: "1:972914232528:web:01a62fe1bc7c05575762d5"
 };

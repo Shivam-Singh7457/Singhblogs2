e2d2952ef3b26401e93a7b9e863b3da1a7b0abd1
Shivam Singh <shivamsinghsn95@gmail.com>
2025-08-06 01:08:42 +0530

    Update PostPage.jsx

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
 import PostCard from '../components/PostCard';
+import { useSelector } from 'react-redux';
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -11,44 +12,55 @@ export default function PostPage() {
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
   const [recentPosts, setRecentPosts] = useState(null);
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchPost = async () => {
       try {
         setLoading(true);
-        const res = await fetch(`${import.meta.env.VITE_API_URL}/api/post/getposts?slug=${postSlug}`);
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/post/getposts?slug=${postSlug}`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              ...(currentUser?.token && {
+                Authorization: `Bearer ${currentUser.token}`,
+              }),
+            },
+          }
+        );
         const data = await res.json();
         if (!res.ok) {
           setError(true);
           setLoading(false);
           return;
         }
-        if (res.ok) {
-          setPost(data.posts[0]);
-          setLoading(false);
-          setError(false);
-        }
+        setPost(data.posts[0]);
+        setError(false);
+        setLoading(false);
       } catch (error) {
         setError(true);
         setLoading(false);
       }
     };
     fetchPost();
-  }, [postSlug]);
+  }, [postSlug, currentUser]);
 
   useEffect(() => {
-    try {
-      const fetchRecentPosts = async () => {
-        const res = await fetch(`${import.meta.env.VITE_API_URL}/api/post/getposts?limit=3`);
+    const fetchRecentPosts = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/post/getposts?limit=3`
+        );
         const data = await res.json();
         if (res.ok) {
           setRecentPosts(data.posts);
         }
-      };
-      fetchRecentPosts();
-    } catch (error) {
-      console.log(error.message);
-    }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchRecentPosts();
   }, []);
 
   if (loading)
@@ -57,44 +69,52 @@ export default function PostPage() {
         <Spinner size='xl' />
       </div>
     );
+
   return (
     <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
       <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
-        {post && post.title}
+        {post?.title}
       </h1>
+
       <Link
-        to={`/search?category=${post && post.category}`}
+        to={`/search?category=${post?.category}`}
         className='self-center mt-5'
       >
         <Button color='gray' pill size='xs'>
-          {post && post.category}
+          {post?.category}
         </Button>
       </Link>
+
       <img
-        src={post && post.image}
-        alt={post && post.title}
+        src={post?.image}
+        alt={post?.title}
         className='mt-10 p-3 max-h-[600px] w-full object-cover'
       />
+
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
-        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span>{new Date(post?.createdAt).toLocaleDateString()}</span>
         <span className='italic'>
-          {post && (post.content.length / 1000).toFixed(0)} mins read
+          {(post?.content.length / 1000).toFixed(0)} mins read
         </span>
       </div>
+
       <div
         className='p-3 max-w-2xl mx-auto w-full post-content'
-        dangerouslySetInnerHTML={{ __html: post && post.content }}
+        dangerouslySetInnerHTML={{ __html: post?.content }}
       ></div>
+
       <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      <CommentSection postId={post._id} />
+
+      <CommentSection postId={post?._id} />
 
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
-          {recentPosts &&
-            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+          {recentPosts?.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
         </div>
       </div>
     </main>
